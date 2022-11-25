519cb76b9482782f0abc59b0ec43a11070a08e06
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-11-25 17:05:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
-
+int numeridiFibonacci(int n){
+    int a=0, b=1, c=0;
+        cout<<b<<endl;
+        while (c<n) {
+         c=a+b;
+         a=b;
+         b=c;
+         cout<<c<<endl;
+        }
+        return n;
+}
 int main() {
-   return 0;
+    float p;
+    cout << "dimmi un numero:"<< endl;
+    cin >> p;
+    int h = numeridiFibonacci(p);
+    return 0;
 }
